9ea930024378f348d424c4be3053633524abcf7f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-05 15:35:55 +0900

    chore

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const Web3 = require("web3");
-// const ethNetwork = "https://rinkeby.infura.io/v3/a8e47da83b924870bc950481841a3d3e"
 const ethNetwork = "https://rinkeby.infura.io/v3/"+process.env.PROJECT_ID
 
 
