455913bc90969988ac97a3a3b2a5c3fa94e3cf53
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-10 13:17:45 +0900

    improve

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const ethNetwork = "https://rinkeby.infura.io/v3/"+process.env.PROJECT_ID
 
 
 try {
-        console.log(process.env.PROJECT_ID)
-        const web3 = new Web3(new Web3.providers.HttpProvider(ethNetwork));
-        console.log("Connection Successfull!");
-        console.log("Latest Block Number: ");
-        web3.eth.getBlockNumber().then(console.log);
-}
-catch(e) {
-        console.log("Connection Error!", e);
+  const web3 = new Web3(new Web3.providers.HttpProvider(ethNetwork));
+  console.log("Connection Successfull!");
+  console.log("Latest Block Number: ");
+  web3.eth.getBlockNumber().then(console.log);
+} catch(e) {
+ console.log("Connection Error!", e);
 }
